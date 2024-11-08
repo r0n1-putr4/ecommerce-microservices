730f3ad948d4d0188fe23df9cf1cb4cd0711b405
r0n1-putr4 <82272376+r0n1-putr4@users.noreply.github.com>
2024-11-08 17:11:39 +0700

    Update app.js

--- product-service/app.js
+++ product-service/app.js
@@ -1,45 +1,49 @@
 const express = require("express");
 const app = express();
 
-//dummy data
+// dummy data
 products = [
   {
     id: 1,
-    name: "Macbook Pro M2",
-    description: "This is a Macbook Pro M2",
-    price: 20000000,
+    name: "Produk 1",
+    description: "Ini adalah produk 1",
+    price: 1000,
   },
   {
     id: 2,
-    name: "Iphone 16 Pro",
-    description: "This is an Iphone 16 Pro",
-    price: 15000000,
+    name: "Produk 2",
+    description: "Ini adalah produk 2",
+    price: 2000,
   },
   {
     id: 3,
-    name: "Samsung Galaxy S20",
-    description: "This is a Samsung Galaxy S20",
-    price: 12000000,
+    name: "Produk 3",
+    description: "Ini adalah produk 3",
+    price: 3000,
   },
 ];
 
+// get all data products
 app.get("/products", (req, res) => {
   res.send(products);
 });
 
+// get single data products by id
 app.get("/products/:id", (req, res) => {
   const id = req.params.id;
-  const product = products.find((product) => product.id == id);
+  const product = products.find((p) => p.id == id);
 
   if (!product) {
     return res.status(404).json({
-      message: "Product not found",
+      success: 0,
+      message: "Data tidak ditemukan",
     });
   }
 
-  req.send(product);
+  res.send(product);
 });
 
+// server running
 app.listen(3000, () => {
-  console.log("Product service is running http://localhost:3000 on port 3000");
+  console.log("Server running http://localhost:3000/products");
 });
