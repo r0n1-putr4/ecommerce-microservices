44fe8b9ddd672e0a6c7a7eb5f01e6bc39fe244fd
r0n1-putr4 <82272376+r0n1-putr4@users.noreply.github.com>
2024-11-20 08:52:51 +0700

    perbaikan code

--- main-app/app.py
+++ main-app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify, render_template # type: ignore
-import requests # type: ignore
+from flask import Flask, jsonify, render_template 
+import requests 
 
 app = Flask(__name__)
 
@@ -18,7 +18,7 @@ def get_sold_products(product_id):
         #folder cart-service
         response = requests.get(f'http://localhost:3001/carts/{product_id}')
         response.raise_for_status()
-        return response.json().get('quantity', 0)
+        return response.json().get('quantity', 0) 
     except requests.exceptions.RequestException as e:
         print(f"Error fetching sold product data: {e}")
         return {"error": "Failed to fetch sold product data"}
@@ -26,7 +26,7 @@ def get_sold_products(product_id):
 def get_reviews(product_id):
     try:
          #folder review-service
-        response = requests.get(f'http://127.0.0.1:3002/products/reviews/{product_id}')
+        response = requests.get(f'http://localhost:3002/products/reviews/{product_id}')
         response.raise_for_status()
         return response.json().get('data', {})
     except requests.exceptions.RequestException as e:
@@ -44,7 +44,10 @@ def get_product_info(product_id):
     # get reviews
     review = get_reviews(product_id)
 
+    #debug isi data berupa json
+    # return jsonify(product, cart, review) 
+
     return render_template('product.html', product=product, cart=cart, review=review)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3003)
+    app.run(debug=True, port=3003, host="0.0.0.0")
